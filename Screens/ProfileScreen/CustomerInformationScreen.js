8db780d404163f1b87b8a284f495c50a4cfd0956
Jeff GBANZIALI <jeffgbanziali@MBP-de-Jeff.lan>
2024-05-03 12:14:15 +0200

    updating

--- Screens/ProfileScreen/CustomerInformationScreen.js
+++ Screens/ProfileScreen/CustomerInformationScreen.js
@@ -1,4 +1,4 @@
-import { View, SafeAreaView, Text, Dimensions, Pressable, TextInput } from 'react-native'
+import { View, SafeAreaView, Text, Dimensions, Pressable, TextInput, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { useNavigation } from '@react-navigation/native';
@@ -26,7 +26,6 @@ const CustomerInformationScreen = () => {
 
     const data = userData.customerData
 
-    console.log('toi', data.first_name)
 
 
     return (
@@ -71,143 +70,437 @@ const CustomerInformationScreen = () => {
 
             </View>
 
+            <ScrollView>
+                <View
+                    style={{
+                        width: "100%",
+                        height: 350,
+                        alignItems: "center",
+                        justifyContent: "space-evenly"
+                    }}>
+
+                    <View
+                        style={{
+                            width: 380,
+                            height: 60,
+                            borderRadius: 4,
+                            flexDirection: "row",
+                            borderWidth: 1,
+                            borderColor: "gray",
+                            backgroundColor: "white"
+
+                        }} >
+
+                        <TextInput
+                            style={{
+                                width: "90%",
+                                height: "100%",
+                                paddingLeft: 12,
+                                fontSize: 20,
+                                color: 'black',
+                            }}
+                            onChangeText={(text) => setEmail(text)}
+                            value={email}
+                            fontSize={20}
+                            placeholder={data.username}
+                            placeholderTextColor="black"
+                            autoCapitalize="none"
+
+                        />
+
+
+
+                    </View>
+
+                    <View
+                        style={{
+                            width: 380,
+                            height: 60,
+                            borderRadius: 4,
+                            flexDirection: "row",
+                            borderWidth: 1,
+                            borderColor: "gray",
+                            backgroundColor: "white"
+
+                        }} >
+
+                        <TextInput
+                            style={{
+                                width: "90%",
+                                height: "100%",
+                                paddingLeft: 12,
+                                fontSize: 20,
+                                color: 'black',
+                            }}
+                            onChangeText={(text) => setEmail(text)}
+                            value={email}
+                            placeholder={data.first_name}
+                            fontSize={20}
+                            placeholderTextColor="black"
+                            autoCapitalize="none"
+
+                        />
+
+
+
+                    </View>
+
+                    <View
+                        style={{
+                            width: 380,
+                            height: 60,
+                            borderRadius: 4,
+                            flexDirection: "row",
+                            borderWidth: 1,
+                            borderColor: "gray",
+                            backgroundColor: "white"
+
+                        }} >
+
+                        <TextInput
+                            style={{
+                                width: "90%",
+                                height: "100%",
+                                paddingLeft: 12,
+                                fontSize: 20,
+                                color: 'black',
+                            }}
+                            onChangeText={(text) => setEmail(text)}
+                            value={email}
+                            placeholder={data.last_name}
+                            fontSize={20}
+                            placeholderTextColor="black"
+                            autoCapitalize="none"
+
+                        />
+
+
+
+                    </View>
+
+                    <View
+                        style={{
+                            width: 380,
+                            height: 60,
+                            borderRadius: 4,
+                            flexDirection: "row",
+                            borderWidth: 1,
+                            borderColor: "gray",
+                            backgroundColor: "white"
+
+                        }} >
+
+                        <TextInput
+                            style={{
+                                width: "90%",
+                                height: "100%",
+                                paddingLeft: 12,
+                                fontSize: 20,
+                                color: 'black',
+                            }}
+                            onChangeText={(text) => setEmail(text)}
+                            value={email}
+                            placeholder={data.email}
+                            fontSize={20}
+                            placeholderTextColor="black"
+                            autoCapitalize="none"
+
+                        />
+
+
+
+                    </View>
+
+                    <View
+                        style={{
+                            width: 380,
+                            height: 60,
+                            borderRadius: 4,
+                            flexDirection: "row",
+                            borderWidth: 1,
+                            borderColor: "gray",
+                            backgroundColor: "white"
+
+                        }} >
+
+                        <TextInput
+                            style={{
+                                width: "90%",
+                                height: "100%",
+                                paddingLeft: 12,
+                                fontSize: 20,
+                                color: 'black',
+                            }}
+                            onChangeText={(text) => setEmail(text)}
+                            value={email}
+                            placeholder={data.billing.phone === "" ? "Votre numéro de téléphone" : data.billing.phone}
+                            fontSize={20}
+                            placeholderTextColor="black"
+                            autoCapitalize="none"
+
+                        />
+
+
+
+                    </View>
+
+                </View>
+
+                <View
+                    style={{
+                        width: "100%",
+                        height: 150,
+                        alignItems: "center",
+                        justifyContent: "space-evenly"
+                    }}>
+
+
+                    <View
+                        style={{
+                            width: "100%",
+                            height: 40,
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}>
+                        <Pressable
+                            style={{
+
+                                borderBottomWidth: 2,
+
+                            }}>
+                            <Text
+                                style={{
+                                    fontSize: 18,
+                                    fontWeight: "500",
+                                    color: "black",
+
+                                }}>
+                                Modifier mon adresse email
+
+                            </Text>
+                        </Pressable>
+
+                    </View>
+
+                    <View
+                        style={{
+                            width: "100%",
+                            height: 40,
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}>
+                        <Pressable
+                            style={{
+
+                                borderBottomWidth: 2,
+
+                            }}>
+                            <Text
+                                style={{
+                                    fontSize: 18,
+                                    fontWeight: "500",
+                                    color: "black",
+
+                                }}>
+                                Modifier mon mot de passe
+
+                            </Text>
+                        </Pressable>
+
+                    </View>
+
+                    <View
+                        style={{
+                            width: "100%",
+                            height: 40,
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}>
+                        <Pressable
+                            style={{
+
+                                borderBottomWidth: 2,
+                                borderColor: "red"
+
+                            }}>
+                            <Text
+                                style={{
+                                    fontSize: 18,
+                                    fontWeight: "500",
+                                    color: "red",
+
+                                }}>
+                                Supprimer mon compte
+
+                            </Text>
+                        </Pressable>
+
+                    </View>
+
+
+
+
+                </View>
+
+                <View
+                    style={{
+                        width: "100%",
+                        height: 150,
+                        alignItems: "center",
+                        justifyContent: "space-evenly"
+                    }}>
+
+
+                    <View
+                        style={{
+                            width: "98%",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            paddingLeft: 5
+                        }}>
+
+                        <Text
+                            style={{
+                                fontSize: 15,
+                                fontWeight: "500",
+                                color: "black",
+                                textAlign: "left"
+
+                            }}>
+                            RECEVEZ NOS INVITATIONS : NOTRE NEWSLETTER POUR ÊTRE AU COURANT DE TOUT, TOUT LE MONDE
+
+                        </Text>
+                    </View>
+
+
+                    <View
+                        style={{
+                            width: "98%",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            paddingLeft: 5
+                        }}>
+
+                        <Text
+                            style={{
+                                fontSize: 13,
+                                fontWeight: "500",
+                                color: "black",
+                                textAlign: "left"
+
+                            }}>
+                            Notre newsletter pour être au courant de tout avant tout le monde
+
+                        </Text>
+                    </View>
+
+                    <View
+                        style={{
+                            width: "50%",
+                            justifyContent: "space-around",
+                            alignItems: "center",
+                            flexDirection: "row"
+                        }}>
+
+
+
+                        <View
+                            style={{
+                                flexDirection: "row",
+                                alignItems: "center",
+                                width: 100,
+                            }}>
+                            <View
+                                style={{
+                                    width: 40,
+                                    height: 40,
+                                    borderRadius: 30,
+                                    backgroundColor: "green",
+
+                                }}>
+
+                            </View>
+                            <Text
+                                style={{
+                                    fontSize: 20,
+                                    fontWeight: "500",
+                                    paddingLeft: 10,
+                                    color: "black",
+
+                                }}>
+                                Oui
+                            </Text>
+                        </View>
+
+                        <View
+                            style={{
+                                flexDirection: "row",
+                                alignItems: "center",
+                                width: 100,
+                            }}>
+                            <View
+                                style={{
+                                    width: 40,
+                                    height: 40,
+                                    borderRadius: 30,
+                                    backgroundColor: "green",
+
+                                }}>
+
+                            </View>
+                            <Text
+                                style={{
+                                    fontSize: 20,
+                                    fontWeight: "500",
+                                    paddingLeft: 10,
+                                    color: "black",
+                                    textAlign: "left"
+
+                                }}>
+
+                                Non
+
+                            </Text>
+                        </View>
+
+                    </View>
+
+                </View>
+
+
+            </ScrollView>
+
             <View
                 style={{
                     width: "100%",
-                    height: 300,
+                    height: 80,
+                    justifyContent: "center",
                     alignItems: "center",
-                    justifyContent:"space-evenly"
                 }}>
-
-                <View
+                <Pressable
                     style={{
-                        width: 380,
+                        width: 300,
                         height: 50,
                         borderRadius: 10,
-                        flexDirection: "row",
-                        borderWidth: 1,
-                        borderColor: "gray",
-                        backgroundColor: "white"
-
-                    }} >
-
-                    <TextInput
+                        justifyContent: "center",
+                        backgroundColor: "black",
+                        alignItems: "center",
+                    }}>
+                    <Text
                         style={{
-                            width: "90%",
-                            height: "100%",
-                            paddingLeft: 12,
                             fontSize: 18,
-                            color: 'black',
-                        }}
-                        onChangeText={(text) => setEmail(text)}
-                        value={email}
-                        placeholder={data.username}
-                        placeholderTextColor="black"
-                        autoCapitalize="none"
+                            fontWeight: "500",
+                            color: "white",
 
-                    />
+                        }}>
+                        Me déconnecter
 
-
-
-                </View>
-
-                <View
-                    style={{
-                        width: 380,
-                        height: 50,
-                        borderRadius: 10,
-                        flexDirection: "row",
-                        borderWidth: 1,
-                        borderColor: "gray",
-                        backgroundColor: "white"
-
-                    }} >
-
-                    <TextInput
-                        style={{
-                            width: "90%",
-                            height: "100%",
-                            paddingLeft: 12,
-                            fontSize: 18,
-                            color: 'black',
-                        }}
-                        onChangeText={(text) => setEmail(text)}
-                        value={email}
-                        placeholder={data.first_name}
-                        placeholderTextColor="black"
-                        autoCapitalize="none"
-
-                    />
-
-
-
-                </View>
-
-                <View
-                    style={{
-                        width: 380,
-                        height: 50,
-                        borderRadius: 10,
-                        flexDirection: "row",
-                        borderWidth: 1,
-                        borderColor: "gray",
-                        backgroundColor: "white"
-
-                    }} >
-
-                    <TextInput
-                        style={{
-                            width: "90%",
-                            height: "100%",
-                            paddingLeft: 12,
-                            fontSize: 18,
-                            color: 'black',
-                        }}
-                        onChangeText={(text) => setEmail(text)}
-                        value={email}
-                        placeholder={data.last_name}
-                        placeholderTextColor="black"
-                        autoCapitalize="none"
-
-                    />
-
-
-
-                </View>
-
-                <View
-                    style={{
-                        width: 380,
-                        height: 50,
-                        borderRadius: 10,
-                        flexDirection: "row",
-                        borderWidth: 1,
-                        borderColor: "gray",
-                        backgroundColor: "white"
-
-                    }} >
-
-                    <TextInput
-                        style={{
-                            width: "90%",
-                            height: "100%",
-                            paddingLeft: 12,
-                            fontSize: 18,
-                            color: 'black',
-                        }}
-                        onChangeText={(text) => setEmail(text)}
-                        value={email}
-                        placeholder={data.email}
-                        placeholderTextColor="black"
-                        autoCapitalize="none"
-
-                    />
-
-
-
-                </View>
+                    </Text>
+                </Pressable>
 
             </View>
+
+
 
 
 
